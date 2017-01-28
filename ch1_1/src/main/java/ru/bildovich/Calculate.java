47fd27eae6dd4c1e0e7576a02a1990b634555aca
hootor <bildovich@mail.ru>
2017-01-28 23:02:52 +1000

    ch1_1

---
+++ ch1_1/src/main/java/ru/bildovich/Calculate.java
@@ -0,0 +1,12 @@
+package ru.bildovich;
+
+public class Calculate{
+public String echo(String value){
+	return String.format("%s %s %s", value, value, value);
+}
+    public static void main(String[] args) {
+		Calculate calc = new Calculate();
+        System.out.println(calc.echo("ahh"));
+        
+    }
+}
