bbe65a5cd8d9a04c1bd65f01c4c9079e2a578ca1
hootor <bildovich@mail.ru>
2017-01-30 22:37:26 +1000

    1_2

---
+++ ch1_2/src/ru/bildovich/Calculate.java
@@ -0,0 +1,8 @@
+package ru.bildovich;
+
+public class Calculate{
+    public static void main(String[] args) {
+		Calculate calc = new Calculate();
+        System.out.println("Hello world");
+    }
+}
