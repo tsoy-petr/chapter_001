721d9a12c825a92c8329d05db035d314649c1e0c
hootor <bildovich@mail.ru>
2017-02-18 00:12:12 +1000

    edit ch4_3

--- ch4_3/src/main/java/ru/bildovich/Paint.java
+++ ch4_3/src/main/java/ru/bildovich/Paint.java
@@ -1,19 +1,32 @@
 package ru.bildovich;
 
 /**
- * Created by mac on 03.02.17.
+ * Class Paint.
+ *
+ * @autor: bildovich
+ * @since: 17.02.2017
  */
 public class Paint {
 
-    String piramid(int h){
-        int center = h/2;
+    /**
+     * Метод выводящий пирамиду из сомволов '*'.
+     *
+     * @param h высота пирамиды
+     * @return строка содержащая пирамиду
+     */
+    public String piramid(int h) {
         StringBuilder sb = new StringBuilder();
-        h=h*2;
-        if(h%2 == 0) h++;
-        for(int i=0; i<h; i+=2)
-        {
-            for(int j=0; j<(h-1)/2-(i/2); j++) sb.append(" ");
-            for(int j=0; j<i+1; j++) sb.append('*');
+        h = h * 2;
+        if (h % 2 == 0) {
+            h++;
+        }
+        for (int i = 0; i < h; i += 2) {
+            for (int j = 0; j < (h - 1) / 2 - (i / 2); j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                sb.append('*');
+            }
             sb.append("\n");
         }
         return sb.toString();
--- ch4_3/src/test/java/ru/bildovich/PaintTest.java
+++ ch4_3/src/test/java/ru/bildovich/PaintTest.java
@@ -3,28 +3,34 @@ package ru.bildovich;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-import static org.hamcrest.number.IsCloseTo.closeTo;
-
+/**
+ * Тест Class Paint.
+ *
+ * @autor: bildovich
+ * @since: 17.02.2017
+ */
 public class PaintTest {
+
+    /**
+     * Тест метода возвращающий строку содеожащую приамиду.
+     *
+     * @throws Exception assertThat
+     */
     @Test
     public void addPiramid() throws Exception {
-        String a =  "          *\n" +
-                    "         ***\n" +
-                    "        *****\n" +
-                    "       *******\n" +
-                    "      *********\n" +
-                    "     ***********\n" +
-                    "    *************\n" +
-                    "   ***************\n" +
-                    "  *****************\n" +
-                    " *******************\n" +
-                    "*********************\n";
+        String a = "          *\n"
+                +  "         ***\n"
+                +  "        *****\n"
+                +  "       *******\n"
+                +  "      *********\n"
+                +  "     ***********\n"
+                +  "    *************\n"
+                +  "   ***************\n"
+                +  "  *****************\n"
+                +  " *******************\n"
+                +  "*********************\n";
         Paint paint = new Paint();
         String result = paint.piramid(10);
         assertThat(result, is(a));
