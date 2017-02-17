ec34904b5513f5f662214c7df0d5c10708c7b394
hootor <bildovich@mail.ru>
2017-02-17 17:16:51 +1000

    edit 3_2, add javaDoc

--- ch3_2/src/main/java/ru/bildovich/Triangle.java
+++ ch3_2/src/main/java/ru/bildovich/Triangle.java
@@ -1,16 +1,31 @@
 package ru.bildovich;
 
+/**
+ * Class Triangle
+ * @autor bildovich
+ * @since 17.02.2017
+ */
 public class Triangle {
     public Point a;
     public Point b;
     public Point c;
 
+    /**
+     * Конструктор с параметрами
+     * @param a
+     * @param b
+     * @param c
+     */
     public Triangle(Point a, Point b, Point c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
+    /**
+     * Метод вычисляющий площадь треугольника
+     * @return площадь треугольника
+     */
     public double area() {
         double area = -1;
         if(area == -1) {
